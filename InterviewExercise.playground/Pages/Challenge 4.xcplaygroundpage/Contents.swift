daacd049d23a64236580238c3c3ba9708bd10531
jbenetts <100961174+jbenetts@users.noreply.github.com>
2025-01-24 12:29:48 -0800

    Add files via upload

---
+++ InterviewExercise.playground/Pages/Challenge 4.xcplaygroundpage/Contents.swift
@@ -0,0 +1,39 @@
+/*:
+ # ZigZag Conversion
+
+ `The string "INSULETISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)`
+
+     I   L   S   I
+     N U E I H R N
+     S   T   I   G
+
+`And then read line by line: "ILSINUEIHRNSTIG".`
+
+ `Write the code that will take a string and make this conversion given a number of rows:
+ string convert(string s, int numRows)`
+
+
+     Input: s = "INSULETISHIRING", numRows = 3
+     Output: "ILSINUEIHRNSTIG"
+
+ ---
+     Input: s = "INSULETISHIRING", numRows = 4
+     Output: "ITINEIRNSLSIGUH"
+
+     I     T    I
+     N   E I  R N
+     S L   S I  G
+     U     H
+
+ ---
+
+ >Constraints:
+ > - 1 <= s.count <= 1000
+ > - s consists of English letters (lower-case and upper-case), ',' and '.'.
+ > - 1 <= numRows <= 1000
+ */
+import Foundation
+
+func convert(_ s: String, _ numRows: Int) -> String {
+    return ""
+}
